8c236e3a865a2ec280986579b3675f4e274df423
u208978 <u208978@s005-n007.aidevcloud>
2023-12-20 03:12:48 -0800

    Update script

--- install-tools.sh
+++ install-tools.sh
@@ -13,8 +13,14 @@
 ./install-tool.sh oneapi-esimd
 ./install-tool.sh oneapi-support
 
+# Add tools
+export T2S_PATH="$( cd "$(dirname $(realpath "$BASH_SOURCE") )" >/dev/null 2>&1 ; pwd -P )" # The path to this script
+TOOLS_PATH=$T2S_PATH/install
+GCC_PATH=$TOOLS_PATH/gcc-7.5.0
+export PATH=$TOOLS_PATH/bin:$GCC_PATH/bin:$PATH
+export LD_LIBRARY_PATH=$TOOLS_PATH/lib:$GCC_PATH/lib64:$LD_LIBRARY_PATH
+
 # Checkout all files with git-lfs installed
-PATH+=:$(pwd)/install/bin
 git checkout -f HEAD
 
 # Compile
